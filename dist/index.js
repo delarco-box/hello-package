75731e36a7f424a0c6e476e9c95bcfb937f0210e
Leandro Del Arco <leandrodelarco@outlook.com>
2023-07-01 17:46:29 -0300

    Add dist

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.hello = void 0;
+var hello_file_1 = require("./hello-file");
+Object.defineProperty(exports, "hello", { enumerable: true, get: function () { return hello_file_1.hello; } });
